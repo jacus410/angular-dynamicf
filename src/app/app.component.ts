6853fe1a8e15df77f4fa7cc9f9fc23a8fbd4400d
jacus410 <jacekj@pro.onet.pl>
2019-06-05 14:59:55 +0200

    2 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component }       from '@angular/core';
-
+import { QuestionBase } from './question-base';
 import { QuestionService } from './question.service';
 
 @Component({
@@ -14,10 +14,76 @@ import { QuestionService } from './question.service';
 })
 export class AppComponent {
   questions: any[];
+  questions2: QuestionBase<any>[] = [
+
+      new DropdownQuestion({
+        key: 'brave',
+        label: 'Bravery Rating',
+        options: [
+          {key: 'solid',  value: 'Solid'},
+          {key: 'great',  value: 'Great'},
+          {key: 'good',   value: 'Good'},
+          {key: 'unproven', value: 'Unproven'}
+        ],
+        order: 4
+      }),
+
+      new TextboxQuestion({
+        key: 'firstName',
+        label: 'First name2',
+        value: 'Bombasto',
+        required: true,
+        order: 5,
+        class: 'red',
+      }),
+
+     new DropdownQuestion({
+        key: 'brave',
+        label: 'Bravery Rating',
+        options: [
+          {key: 'solid',  value: 'Solid'},
+          {key: 'great',  value: 'Great'},
+          {key: 'good',   value: 'Good'},
+          {key: 'unproven', value: 'Unproven'}
+        ],
+        order: 6
+      }),      
+      
+       new TextboxQuestion({
+        key: 'firstName',
+        label: 'First name',
+        value: 'Bombasto',
+        required: true,
+        order: 3,
+        class: 'red',
+      }),
+
+       new TextboxQuestion({
+        key: 'secondName',
+        label: 'First',
+        type: 'number',
+        value: 1,
+        required: false,
+        order: 2,
+        class: 'red'
+      }),
+
+      new TextboxQuestion({
+        key: 'emailAddress',
+        label: 'Email',
+        type: 'radio',
+        order: 1,
+        class: 'red',
+      })
+    ]
 
   constructor(service: QuestionService) {
     this.questions = service.getQuestions();
   }
+
+
+
+
 }
 
 
